823ab145b9782a3e995c7044043010b222e24ab7
milano12345 <dmilano857@gmail.com>
2019-09-20 11:19:39 -0500

    added axios call with auth to bubblepage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "./AxiosWithAuth";
+import { Redirect } from "react-router-dom";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [credentials, setCredentials] = useState({});
@@ -12,6 +14,7 @@ const Login = () => {
       .then(response => {
         console.log(response);
         localStorage.setItem("token", response.data.payload);
+        props.history.push("/bubblepage");
       })
       .catch(err => console.log(err));
   };
